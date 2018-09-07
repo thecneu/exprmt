182158346573dee1e45a80ca903c619339c038fe
Corey Newhouse <corey.newhouse@deutschinc.com>
2018-09-06 21:46:22 -0700

    fix some styling issues

--- src/components/filter/FilterAttribute/index.jsx
+++ src/components/filter/FilterAttribute/index.jsx
@@ -6,11 +6,11 @@ import styles from './filter-attribute.module.scss'
 
 const FilterAttribute = ({ selected, attribute, isSwatch, updateAppliedFilter }) => (
   <div className={cx(styles.attribute, { [styles.swatch]: isSwatch })} onClick={() => updateAppliedFilter(attribute)}>
-    <label className={cx({ [styles.selected]: isSwatch && selected })}>
-      {!isSwatch && <input type="checkbox" checked={selected} />}
+    <div className={cx(styles.label, { [styles.selected]: isSwatch && selected })}>
+      {!isSwatch && <input type="checkbox" checked={selected} onClick={() => updateAppliedFilter(attribute)} />}
       {attribute.color && <ColorBox color={attribute.color} isSwatch={isSwatch} selected={selected} />}
       {!isSwatch && <DisplayHtml className={styles.value}>{attribute.value}</DisplayHtml>}
-    </label>
+    </div>
   </div>
 )
 
